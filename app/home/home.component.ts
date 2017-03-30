427ccfe5c4f0eb5c2615ee4115ee19b0539c8b23
AJ DeTorrice <ajdetorrice@hotmail.com>
2017-03-29 20:42:26 -0500

    Initial GUI commit

---
+++ app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home',
+  templateUrl: './app/home/home.component.html',
+  styleUrls: [ './app/home/home.component.css' ]
+})
+
+export class HomeComponent { 
+	@Output() titleUpdated : EventEmitter<string> = new EventEmitter();
+	title : string;
+
+	constructor(){
+		this.title = "Login/SignUp";
+		this.titleUpdated.emit(this.title);
+	}
+}
