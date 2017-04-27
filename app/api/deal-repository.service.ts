e7564a3ac99074ecb3cd643ba545cc22d3e27ec2
AJ DeTorrice <ajdetorrice@hotmail.com>
2017-04-27 14:48:15 -0500

    Added api calls to deal repo service

--- app/api/deal-repository.service.ts
+++ app/api/deal-repository.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 import { Deal } from './deal';
 
 @Injectable()
 export class DealRepository {
+    private _apiUrl = 'api/deals';
 
 	private _deals: Deal[];
 
@@ -16,35 +18,74 @@ export class DealRepository {
 		return -1;
 	}
 
-	constructor(){
+    constructor(private http: Http){
 		this._deals = [
 			{ id: 1, title: 'Deal 1', description: "SAMPLE DESCRIPTION", imagePath: 'img/tilted.png', rating: 20 },
 			{ id: 2, title: 'Deal 2', description: "SAMPLE DESCRIPTION", imagePath: 'img/unknown.png', rating: 35 },
 			{ id: 3, title: 'Deal 3', description: "SAMPLE DESCRIPTION", imagePath: 'img/wat.png', rating: 10 }
 		];
-	}
+    }
 
-	public list() : Deal[] {
+    public list(): Promise<Deal[]> {
+        return this.http
+            .get(this._apiUrl)
+            .toPromise()
+            .then(x => x.json().data as Deal[])
+            .catch(x => x.message);
+    }
+
+	public listInternal() : Deal[] {
 		return this._deals;
-	}
+    }
 
-	public get(id : number) : Deal {
+    public get(id: number): Promise<Deal> {
+        return this.http
+            .get(`${this._apiUrl}/${id}`)
+            .toPromise()
+            .then(x => x.json().data as Deal)
+            .catch(x => x.message);
+    }
+
+	public getInternal(id : number) : Deal {
 		var index = this.getIndex(id);
 		return this._deals[index];
-	}
+    }
 
-	public add(deal: Deal) {
+    public add(deal: Deal): Promise<Deal> {
+        return this.http
+            .post(this._apiUrl, deal)
+            .toPromise()
+            .then(x => x.json().data as Deal)
+            .catch(x => x.message);
+    }
+
+	public addInternal(deal: Deal) {
 		deal.id = this._deals.length + 1;
 		this._deals.push(deal);
-	}
+    }
 
-	public update(deal: Deal) {
+    public update(deal: Deal): Promise<Deal> {
+        return this.http
+            .put(`${this._apiUrl}/${deal.id}`, deal)
+            .toPromise()
+            .then(() => deal)
+            .catch(x => x.message);
+    }
+
+	public updateInternal(deal: Deal) {
 		var index = this.getIndex(deal.id);
 		this._deals[index] = deal;
 	}
 
-	public delete(id : number) {
+    public delete(deal: Deal): Promise<void> {
+        return this.http
+            .delete(`${this._apiUrl}/${deal.id}`)
+            .toPromise()
+            .catch(x => x.message);
+    }
+
+	public deleteInternal(id : number) {
 		var index = this.getIndex(id);
 		this._deals.splice(index, 1);
-	}
+    }
 }
