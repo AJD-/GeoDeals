9eb701eafdf573c50eaa7ae64fbf7d01163b1865
AJD- <ajdetorrice@hotmail.com>
2017-04-29 01:50:57 -0500

    Cleaned up deal repo, get now interfaces correctly with the endpoint

--- app/api/deal-repository.service.ts
+++ app/api/deal-repository.service.ts
@@ -6,40 +6,22 @@ import { Deal } from './deal';
 
 @Injectable()
 export class DealRepository {
-    private _apiUrl = 'api/deals';
-    private _testApiUrl = 'http://54.70.252.84/api/deals/test';
+    private _apiUrl = 'http://54.70.252.84/api/deals/search';
 
 	private _deals: Deal[];
 
-	private getIndex(id : number){
-		for (var i = this._deals.length; i--;) {
-			var deal = this._deals[i];
-			if(deal.id == id) return i;
-		}
-		return -1;
-	}
-
     constructor(private http: Http){
-		this._deals = [
-			{ id: 1, title: 'Deal 1', description: "SAMPLE DESCRIPTION", imagePath: 'img/tilted.png', rating: 20 },
-			{ id: 2, title: 'Deal 2', description: "SAMPLE DESCRIPTION", imagePath: 'img/unknown.png', rating: 35 },
-			{ id: 3, title: 'Deal 3', description: "SAMPLE DESCRIPTION", imagePath: 'img/wat.png', rating: 10 }
-		];
     }
 
     public list(): Promise<Deal[]> {
         return this.http
-            .get(this._testApiUrl)
+            .get(this._apiUrl)
             .toPromise()
             .then(x => {
                 let body = x.json();
-                return (body.data || body) as Deal[];
+                return (body.deals.final_deals) as Deal[];
             })
             .catch(x => x.message);
-    }
-
-	public listInternal() : Deal[] {
-		return this._deals;
     }
 
     public get(id: number): Promise<Deal> {
@@ -50,22 +32,12 @@ export class DealRepository {
             .catch(x => x.message);
     }
 
-	public getInternal(id : number) : Deal {
-		var index = this.getIndex(id);
-		return this._deals[index];
-    }
-
     public add(deal: Deal): Promise<Deal> {
         return this.http
             .post(this._apiUrl, deal)
             .toPromise()
             .then(x => x.json().data as Deal)
             .catch(x => x.message);
-    }
-
-	public addInternal(deal: Deal) {
-		deal.id = this._deals.length + 1;
-		this._deals.push(deal);
     }
 
     public update(deal: Deal): Promise<Deal> {
@@ -76,20 +48,10 @@ export class DealRepository {
             .catch(x => x.message);
     }
 
-	public updateInternal(deal: Deal) {
-		var index = this.getIndex(deal.id);
-		this._deals[index] = deal;
-	}
-
     public delete(deal: Deal): Promise<void> {
         return this.http
             .delete(`${this._apiUrl}/${deal.id}`)
             .toPromise()
             .catch(x => x.message);
-    }
-
-	public deleteInternal(id : number) {
-		var index = this.getIndex(id);
-		this._deals.splice(index, 1);
     }
 }
