67b06050482b75f5c3a14d3d4a205bd055c106fe
AJD- <ajdetorrice@hotmail.com>
2017-05-02 11:37:24 -0500

    Add deal now communicates with api, sends store id

--- app/deal/adddeal/add-deal.component.ts
+++ app/deal/adddeal/add-deal.component.ts
@@ -15,14 +15,35 @@ export class AddDealComponent {
     private deal: any = {};
     private loc: any = {};
     private stores: any[];
+    private addedStore: boolean = false;
+    private store: any;
+    private currentDate: any;
 
     constructor(private router: Router,
         private route: ActivatedRoute,
         private dealRepository: DealRepository,
         private http: Http,
-        private locationService: LocationService) { }
+        private locationService: LocationService) {
+
+        //Get current date
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1;
+        var yyyy = today.getFullYear();
+        let day = "";
+        let month = "";
+        if (dd < 10) {
+            day = '0' + dd
+        }
+        if (mm < 10) {
+            month = '0' + mm
+        }
+        let datestr = yyyy + '-' + month + '-' + day;
+        this.currentDate = datestr;
+    }
 
     submit() {
+        console.log(this.deal);
         this.dealRepository.add(this.deal)
             .then(x => this.goToDealDetail('Deal Submitted'));
     }
@@ -33,12 +54,13 @@ export class AddDealComponent {
     }
 
     locationSubmit() {
-        console.log("Location activated with " + this.loc.state + ", " + this.loc.city);
+        console.log("Location activated with: \nState: " + this.loc.state + "\nCity: " + this.loc.city + "\nLatitude: " + this.loc.latitude + "\nLongitude: " + this.loc.longitude);
     }
 
     getLocation() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function (position) {
+                //Hack to get around 'this' being null
                 window.sessionStorage.setItem("latitude", String(position.coords.latitude));
                 window.sessionStorage.setItem("longitude", String(position.coords.longitude));
             });
@@ -50,10 +72,18 @@ export class AddDealComponent {
     }
 
     search(value: string) {
-        console.log("ayy u hit search with search query: " + value);
+        console.log("Sending search out with query: " + value);
         this.loc.store = value;
         console.log(this.loc);
         this.locationService.sendLoc(this.loc)
             .then(x => this.stores = x);
+    }
+
+    //expecting a json object with an id parameter
+    selectedStore(store: any) {
+        this.deal.store_id = store.id;
+        this.store = store;
+        console.log("Added store id: " + this.deal.store_id + " to deal.");
+        this.addedStore = true;
     }
 }
